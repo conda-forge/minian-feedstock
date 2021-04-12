a9a81bfad06fac62efd34b87c4877ab65f8327ea
phildong <zhe.dong@icahn.mssm.edu>
2021-04-12 09:54:15 -0400

    remove post-link scripts

--- recipe/post-link.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-conda install -c bioconda medpy=0.4.0
-conda install -c simpleitk simpleitk=2.0.2
--- recipe/post-link.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-conda install -c bioconda medpy=0.4.0
-conda install -c simpleitk simpleitk=2.0.2
